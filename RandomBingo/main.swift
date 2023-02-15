11ae574bba6c8a5e995819e7a1c674e479b29451
eundeang <2021112027@dgu.ac.kr>
2023-02-15 15:36:11 +0900

    빙고 맥 커맨드 라인 완성

--- RandomBingo/main.swift
+++ RandomBingo/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+var computerChoice = Int.random(in: 1...100)
 
+var myChoise : Int = 0
+
+// 조건에 따른 반복문, 반복횟수를 정확히 알 수 없을때, 제어전송문(break)을 사용해서 정지함
+while true {
+    // 문자열 값으로 받아온다.
+    var userInput = readLine()
+    
+    // if let 바인딩
+    if let input = userInput {
+        if let number = Int(input) {
+            myChoise = number
+        }
+    }
+    
+    
+    if computerChoice > myChoise {
+        print("Up")
+    }else if computerChoice < myChoise{
+        print("Down")
+    }else{
+        print("Bingo")
+        break
+    }
+}
